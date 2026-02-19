a9df7124be362573c7677ce8f750039294059465
JangTaeYong <j01028957795@gmail.com>
2026-02-19 19:36:14 +0900

    [GUI] Log 탭 버튼 추가

--- gui/src/lovo_gui/lovo_gui/tabs/tab06_log/tab06_log.py
+++ gui/src/lovo_gui/lovo_gui/tabs/tab06_log/tab06_log.py
@@ -3,7 +3,7 @@ Log 탭
 """
 from PyQt6.QtWidgets import (
     QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QCalendarWidget,
-    QTextEdit, QSizePolicy, QTableWidget, QTableWidgetItem, QHeaderView
+    QSizePolicy, QTableWidget, QHeaderView
 )
 from PyQt6.QtCore import Qt
 
@@ -14,6 +14,18 @@ class LogTab(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.log_table = None
+        self.filter_buttons = []
+        self.button_handlers = [
+            self.on_click_robot,
+            self.on_click_orders,
+            self.on_click_customer,
+            self.on_click_charging_station,
+            self.on_click_furniture,
+            self.on_click_inventory_tx,
+            self.on_click_robot_job,
+            self.on_click_robot_state_log,
+            self.on_click_fire_log,
+        ]
         self._setup_ui()
     
     def _setup_ui(self):
@@ -27,8 +39,10 @@ class LogTab(QWidget):
         # 오른쪽: 로그 테이블
         self.log_table = QTableWidget()
         self.log_table.setColumnCount(4)
-        self.log_table.setRowCount(25)
+        self.log_table.setRowCount(100)
         self.log_table.setHorizontalHeaderLabels(["시간", "탭1", "탭2", "탭3"])
+        self.log_table.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAsNeeded)
+        self.log_table.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
         
         # 헤더 설정
         header = self.log_table.horizontalHeader()
@@ -77,10 +91,23 @@ class LogTab(QWidget):
         left_layout.addWidget(self.calendar)
         
         # 버튼들
-        for text in ["자재", "모니터링", "알람"]:
+        button_names = [
+            "robot",
+            "orders",
+            "customer",
+            "charging_station",
+            "furniture",
+            "inventory_tx",
+            "robot_job",
+            "robot_state_log",
+            "fire log",
+        ]
+        for text, handler in zip(button_names, self.button_handlers):
             btn = QPushButton(text)
             btn.setFixedWidth(260)
             btn.setFixedHeight(40)
+            btn.clicked.connect(handler)
+            self.filter_buttons.append(btn)
             left_layout.addWidget(btn)
         
         left_layout.addStretch()
@@ -92,3 +119,48 @@ class LogTab(QWidget):
         selected_date = self.calendar.selectedDate().toString("yyyy-MM-dd")
         # TODO: DB에서 해당 날짜의 로그 조회 및 테이블 업데이트
         print(f"📅 [{selected_date}] 날짜 선택됨")
+
+    def on_click_robot(self):
+        """robot 버튼 클릭"""
+        # TODO: robot DB 조회/연결 로직 여기에 작성해라.
+        pass
+
+    def on_click_orders(self):
+        """orders 버튼 클릭"""
+        # TODO: orders DB 조회/연결 로직 여기에 작성해라.
+        pass
+
+    def on_click_customer(self):
+        """customer 버튼 클릭"""
+        # TODO: customer DB 조회/연결 로직 여기에 작성해라.
+        pass
+
+    def on_click_charging_station(self):
+        """charging_station 버튼 클릭"""
+        # TODO: charging_station DB 조회/연결 로직 여기에 작성해라.
+        pass
+
+    def on_click_furniture(self):
+        """furniture 버튼 클릭"""
+        # TODO: furniture DB 조회/연결 로직 여기에 작성해라.
+        pass
+
+    def on_click_inventory_tx(self):
+        """inventory_tx 버튼 클릭"""
+        # TODO: inventory_tx DB 조회/연결 로직 여기에 작성해라.
+        pass
+
+    def on_click_robot_job(self):
+        """robot_job 버튼 클릭"""
+        # TODO: robot_job DB 조회/연결 로직 여기에 작성해라.
+        pass
+
+    def on_click_robot_state_log(self):
+        """robot_state_log 버튼 클릭"""
+        # TODO: robot_state_log DB 조회/연결 로직 여기에 작성해라.
+        pass
+
+    def on_click_fire_log(self):
+        """fire log 버튼 클릭"""
+        # TODO: fire log DB 조회/연결 로직 여기에 작성해라.
+        pass
